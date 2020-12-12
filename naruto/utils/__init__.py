a1524b84e56f4a68dd1a1a0027d8b6f70088e5d8
edguru <72596185+edguru@users.noreply.github.com>
2020-12-12 15:39:30 +0530

    Add files via upload

---
+++ naruto/utils/__init__.py
@@ -0,0 +1,11 @@
+
+from .progress import progress  # noqa
+from .sys_tools import SafeDict, get_import_path, terminate, secure_text  # noqa
+from .tools import (demojify,  # noqa
+                    get_file_id_and_ref,
+                    humanbytes,
+                    time_formatter,
+                    post_to_telegraph,
+                    runcmd,
+                    take_screen_shot,
+                    parse_buttons)
---
+++ naruto/utils/exceptions.py
@@ -0,0 +1,13 @@
+
+
+
+class StopConversation(Exception):
+    """ raise if conversation has terminated """
+
+
+class ProcessCanceled(Exception):
+    """ raise if thread has terminated """
+
+
+class narutoBotNotFound(Exception):
+    """ raise if naruto bot not found """
