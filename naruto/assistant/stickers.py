3710bf13dcb7ef114103b49f1d0a025bd45ca950
edguru <72596185+edguru@users.noreply.github.com>
2020-12-13 15:07:32 +0530

    Update stickers.py

--- naruto/assistant/stickers.py
+++ naruto/assistant/stickers.py
@@ -6,7 +6,7 @@ import time
 from pyrogram import filters
 from pyrogram.types import ReplyKeyboardMarkup
 
-from nana import setbot, AdminSettings, DB_AVAILABLE, app, Owner
+from nana import setbot, AdminSettings, DB_AVAILABLE, naruto, Owner
 from nana.assistant.database.stickers_db import set_sticker_set, set_stanim_set
 from .settings import get_text_settings, get_button_settings
 
@@ -19,15 +19,15 @@ async def get_stickers(_client, message):
         await message.edit("Your database is not avaiable!")
         return
     global TEMP_KEYBOARD, USER_SET
-    await app.send_message("@Stickers", "/stats")
+    await naruto.send_message("@Stickers", "/stats")
     # app.read_history("@Stickers")
     time.sleep(0.2)
-    keyboard = await app.get_history("@Stickers", limit=1)
+    keyboard = await naruto.get_history("@Stickers", limit=1)
     keyboard = keyboard[0].reply_markup.keyboard
     for x in keyboard:
         for y in x:
             TEMP_KEYBOARD.append(y)
-    await app.send_message("@Stickers", "/cancel")
+    await naruto.send_message("@Stickers", "/cancel")
     msg = await message.reply("Select your stickers for set as kang sticker",
                               reply_markup=ReplyKeyboardMarkup(keyboard))
     USER_SET[message.from_user.id] = msg.message_id
@@ -42,15 +42,15 @@ async def get_stickers_animation(_client, message):
         await message.edit("Your database is not avaiable!")
         return
     global TEMP_KEYBOARD, USER_SET
-    await app.send_message("@Stickers", "/stats")
+    await naruto.send_message("@Stickers", "/stats")
     # app.read_history("@Stickers")
     time.sleep(0.2)
-    keyboard = await app.get_history("@Stickers", limit=1)
+    keyboard = await naruto.get_history("@Stickers", limit=1)
     keyboard = keyboard[0].reply_markup.keyboard
     for x in keyboard:
         for y in x:
             TEMP_KEYBOARD.append(y)
-    await app.send_message("@Stickers", "/cancel")
+    await naruto.send_message("@Stickers", "/cancel")
     msg = await message.reply("Select your stickers for set as kang animation sticker",
                               reply_markup=ReplyKeyboardMarkup(keyboard))
     USER_SET[message.from_user.id] = msg.message_id
@@ -101,11 +101,11 @@ async def settings_sticker(_client, message):
         await message.edit("Your database is not avaiable!")
         return
     global TEMP_KEYBOARD, USER_SET
-    await app.send_message("@Stickers", "/stats")
+    await naruto.send_message("@Stickers", "/stats")
     # app.read_history("@Stickers")
     time.sleep(0.2)
     try:
-        keyboard = await app.get_history("@Stickers", limit=1)
+        keyboard = await naruto.get_history("@Stickers", limit=1)
         keyboard = keyboard[0].reply_markup.keyboard
     except:
         message.message.edit_text("You dont have any sticker pack!\nAdd stickers pack in @Stickers ")
@@ -113,7 +113,7 @@ async def settings_sticker(_client, message):
     for x in keyboard:
         for y in x:
             TEMP_KEYBOARD.append(y)
-    await app.send_message("@Stickers", "/cancel")
+    await naruto.send_message("@Stickers", "/cancel")
     await message.message.delete()
     msg = await setbot.send_message(Owner, "Select your stickers for set as kang animation sticker",
                                     reply_markup=ReplyKeyboardMarkup(keyboard))
